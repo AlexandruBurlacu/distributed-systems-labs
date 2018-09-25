5c6ec23a260708a5f5425db5a36cce0311c6b98b
Alex Burlacu <alexburlacu96@gmail.com>
2018-09-25 12:57:00 +0300

    remove magic method

--- lab1/apps/message_broker/lib/message_broker.ex
+++ lab1/apps/message_broker/lib/message_broker.ex
@@ -6,24 +6,6 @@ defmodule MessageBroker do
     @port 60660
     @ip 'localhost'
 
-    @doc """
-        A test method for the MessageLib.Client within MessageBroker
-    """
-    def magic do
-        sock1 = MessageLib.Client.init 12321
-        sock2 = MessageLib.Client.init 32123
-
-        MessageLib.Client.send sock1, "My Message", {@ip, 32123}
-
-        {msg1, config} = MessageLib.Client.receive # sock2
-
-        MessageLib.Client.send sock2, "My New Message", config
-
-        {msg2, _} = MessageLib.Client.receive # sock1
-
-        IO.inspect {msg1, msg2}
-    end
-
     def init do
         socket = MessageLib.Client.init @port
 
