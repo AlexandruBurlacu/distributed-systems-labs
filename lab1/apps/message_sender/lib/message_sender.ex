77a9e586933af7de5de3c07a5ac4adade955104c
Victor Istratii <victor.istratii@faf.utm.md>
2018-09-25 13:11:06 +0300

    Added a message sender with init and send functions.

---
+++ lab1/apps/message_sender/lib/message_sender.ex
@@ -0,0 +1,16 @@
+defmodule MessageSender do
+  @broker_ip 'localhost'
+  @broker_port 60660
+  @sender_ip 'localhost'
+  @broker_port 13337
+
+  require Logger
+
+  def init(port) do
+    MessageLib.Client.init(port)
+  end
+
+  def send(socket, data) do
+    MessageLib.Client.send(socket, data, {@broker_ip, @broker_port})
+  end
+end
