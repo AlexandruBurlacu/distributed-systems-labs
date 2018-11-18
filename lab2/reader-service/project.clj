ddcb0639c391d548e071ab9b7e8a8c9d0386f732
Alex Burlacu <alexburlacu96@gmail.com>
2018-11-18 14:40:38 +0200

    add dependencies necessary for interacting with the ScyllaDB cluster

--- lab2/reader-service/project.clj
+++ lab2/reader-service/project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [cc.qbits/alia "4.3.0"]
+                 ]
   :main ^:skip-aot reader-service.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- lab2/reader-service/src/reader_service/core.clj
+++ lab2/reader-service/src/reader_service/core.clj
@@ -1,8 +1,18 @@
 (ns reader-service.core
   (:gen-class))
 
+(require '[qbits.alia :as alia])
+(require '[clojure.core.async :as async])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
+  (def cluster (alia/cluster {:contact-points ["lab2_scylladb1_1"]}))
+  (def session (alia/connect cluster))
+  (alia/execute session "CREATE KEYSPACE alia
+                         WITH replication = {'class': 'SimpleStrategy',
+                                             'replication_factor' : 3};")
+  (println "All is done")
+  (alia/shutdown session)
+  (alia/shutdown cluster)
+  (println "Good bye"))
