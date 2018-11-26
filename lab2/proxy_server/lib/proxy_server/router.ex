fcfd12e12290623ab64d0068caf68860f9ffb3dd
Victor Istratii <victor.istratii@faf.utm.md>
2018-11-26 23:52:01 +0200

    Testing several routes.

--- lab2/proxy_server/lib/proxy_server/router.ex
+++ lab2/proxy_server/lib/proxy_server/router.ex
@@ -1,9 +1,28 @@
 defmodule ProxyServer.Router do
   use Plug.Router
 
+  alias ProxyServer.Plug.VerifyRequest
+
+  plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+
+  plug(
+    VerifyRequest,
+    fields: ["content", "mimetype"],
+    paths: ["/upload"]
+  )
+
   plug(:match)
   plug(:dispatch)
 
-  get("/", do: send_resp(conn, 200, "Welcome"))
-  match(_, do: send_resp(conn, 404, "Oops!"))
+  get("/", do: send_resp(conn, 200, "Welcome\n"))
+  get("/upload", do: send_resp(conn, 201, "Uploaded\n"))
+
+  post "/post" do
+    {:ok, body, conn} = read_body(conn)
+    body = Poison.decode!(body)
+    IO.inspect(body)
+    send_resp(conn, 201, "created: #{get_in(body, ["message"])}")
+  end
+
+  match(_, do: send_resp(conn, 404, "Oops!\n"))
 end
