c9efae359ad0d4d39bfc6fb2c5b52bad4c9a80e5
Alex Burlacu <alexburlacu96@gmail.com>
2018-11-27 14:57:55 +0200

    defined multimethod to wirte into db

--- lab2/writer-service/src/writer_service/core.clj
+++ lab2/writer-service/src/writer_service/core.clj
@@ -4,8 +4,35 @@
 (require '[qbits.alia :as alia])
 (require '[clojure.core.async :as async])
 
+(defmulti write-to-db :table)
+;; The `data` should be a dict
+;; with top-level pair :table {:actors, :movies, :studios}
+
+(defmethod write-to-db :actors [data]
+  (def session (:sess data))
+  (def prepared-statement
+    (alia/prepare session "INSERT INTO porndb.actors
+                           (actor_id, name, is_female, age)
+                           VALUES (uuid(), :name, :is_female, :age);"))
+  (alia/execute session prepared-statement {:values (:content data)}))
+
+(defmethod write-to-db :studios [data]
+  (def session (:sess data))
+  (def prepared-statement
+    (alia/prepare session "INSERT INTO porndb.studios
+                           (studio_id, name, movies, actors)
+                           VALUES (uuid(), :name, :movies, :actors);"))
+  (alia/execute session prepared-statement {:values (:content data)}))
+
+(defmethod write-to-db :movies [data]
+  (def session (:sess data))
+  (def prepared-statement
+    (alia/prepare session "INSERT INTO porndb.movies
+                           (movie_id, name, link, length, tags, actors, studio)
+                           VALUES (uuid(), :name, :link, :length, :tags, :actors, :studio);"))
+  (alia/execute session prepared-statement {:values (:content data)}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (def cluster (alia/cluster {:contact-points ["lab2_scylladb1_1"]}))
   (def session (alia/connect cluster))
