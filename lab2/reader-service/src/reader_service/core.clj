bf0f5d6e0219465e9c777d7c4ca74e509d871b04
Alex Burlacu <alexburlacu96@gmail.com>
2018-12-12 13:24:33 +0200

    close issue #8 everything works as required

--- lab2/reader-service/src/reader_service/core.clj
+++ lab2/reader-service/src/reader_service/core.clj
@@ -6,51 +6,58 @@
   (:require [qbits.alia :as alia]
             [clj-json.core :as json]))
 
-(def cluster (alia/cluster {:contact-points ["scylladb1", "scylladb2", "scylladb3"]}))
+(def cluster (alia/cluster {:contact-points ["scylladb1" "scylladb2" "scylladb3"]}))
 
 (defn json-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/json"}
    :body (json/generate-string data)})
 
+(defn adapt-response [data key]
+  (map #(update-in %1 [key] str) data))
+
 (defroutes handler
   (GET "/actors" req
     (let [
       name (get (:query-params req) "name")
+      session (alia/connect cluster)
+      resp (cond
+             (some? name) (do
+                            (let [
+                              prepared-statement
+                              (alia/prepare session "SELECT * FROM porndb.actors
+                                            WHERE name=? ALLOW FILTERING ;")
+                            ]
+                            (alia/execute session prepared-statement {:values [name]})))
+             :else (alia/execute session "SELECT * FROM porndb.actors ;"))
     ]
-    (def session (alia/connect cluster))
-    (def resp
-      (cond
-        (some? name) (do
-                       (def prepared-statement
-                         (alia/prepare session "SELECT * FROM porndb.actors
-                                           WHERE name=? ALLOW FILTERING ;"))
-                       (alia/execute session prepared-statement {:values [name]}))
-        :else (alia/execute session "SELECT * FROM porndb.actors ;")))
     (alia/shutdown session)
-    (json-response {"resp" resp})))
+    (json-response {"resp" (adapt-response resp :actor_id)})))
 
   (GET "/movies" req
     (let [
-      name (get (:query-params req) "name")
       length (get (:query-params req) "length")
+      name (get (:query-params req) "name")
+      session (alia/connect cluster)
+      resp (cond
+             (some? name) (do
+                            (let [
+                              prepared-statement
+                              (alia/prepare session "SELECT * FROM porndb.movies
+                                            WHERE length=0 AND name=? ALLOW FILTERING ;")
+                            ]
+                            (alia/execute session prepared-statement {:values [name]})))
+             (some? length) (do
+                              (let [
+                                prepared-statement
+                                (alia/prepare session "SELECT * FROM porndb.movies
+                                              WHERE length=? ALLOW FILTERING ;")
+                              ]
+                              (alia/execute session prepared-statement {:values [length]})))
+             :else (alia/execute session "SELECT * FROM porndb.movies ;"))
     ]
-    (def session (alia/connect cluster))
-    (def resp
-      (cond
-        (some? name) (do
-                       (def prepared-statement
-                         (alia/prepare session "SELECT * FROM porndb.movies
-                                                WHERE name=? ALLOW FILTERING ;"))
-                       (alia/execute session prepared-statement {:values [name]}))
-        (some? length) (do
-                         (def prepared-statement
-                           (alia/prepare session "SELECT * FROM porndb.movies
-                                                  WHERE length=? ALLOW FILTERING ;"))
-                         (alia/execute session prepared-statement {:values [length]}))
-        :else (alia/execute session "SELECT * FROM porndb.movies ;")))
     (alia/shutdown session)
-    (json-response {"resp" resp}))))
+    (json-response {"resp" (adapt-response resp :actor_id)}))))
 
 (def app
   (-> handler
